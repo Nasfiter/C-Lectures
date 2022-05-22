229b65e7bb2e5a51ccc008b673f302c03214f7c4
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-22 19:53:27 -0400

    Exp005 f-els example is added

--- Exp004_Div/Program.cs
+++ Exp004_Div/Program.cs
@@ -1,4 +1,5 @@
-﻿double numberA = 7;
+﻿int numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
 double numberB = 0.5;
 Console.Write("Result= ");
 Console.WriteLine(numberA / numberB);
